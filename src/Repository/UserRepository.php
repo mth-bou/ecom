1ac6b840c352c457f9cc9d8339607990dd66010a
Mathieu <mathieu.boucher@epsi.fr>
2024-03-27 12:43:08 +0100

    Add Registration and login system for users + google mailer configuration + more security for env vars

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,8 +4,10 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
  * @extends ServiceEntityRepository<User>
@@ -15,82 +17,25 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository implements UserRepositoryInterface
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
     }
 
-    public function save(User $user, bool $flush = false): void
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
     {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $user::class));
+        }
+
+        $user->setPassword($newHashedPassword);
         $this->getEntityManager()->persist($user);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function edit(User $user, array $fields, bool $flush = false): void
-    {
-        foreach ($fields as $fieldName => $fieldValue) {
-            $setter = 'set' . ucfirst($fieldName);
-            if (method_exists($user, $setter)) {
-                $user->$setter($fieldValue);
-            }
-        }
-        $this->getEntityManager()->persist($user);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(User $user, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($user);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    /**
-     * @throws NonUniqueResultException
-     */
-    public function findByEmail(string $email): ?User
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.email = :email')
-            ->setParameter('u.email', $email)
-            ->getQuery()
-            ->getOneOrNullResult();
-    }
-
-    /**
-     * @throws NonUniqueResultException
-     */
-    public function findByLastname(string $lastname): array
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.lastname = :lastname')
-            ->setParameter('u.lastname', $lastname)
-            ->getQuery()
-            ->getOneOrNullResult();
-    }
-
-    /**
-     * @throws NonUniqueResultException
-     */
-    public function findByEmailAndPassword(string $email, string $password): ?User
-    {
-        return $this->createQueryBuilder('u')
-            ->where('u.email = :email')
-            ->andWhere('u.password = :password')
-            ->setParameter('email', $email)
-            ->setParameter('password', $password)
-            ->getQuery()
-            ->getOneOrNullResult();
+        $this->getEntityManager()->flush();
     }
 
 //    /**
